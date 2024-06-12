6ec1278c3445bc65f8d17787d2e0a1548c674f6a
alecbass <alec.bassingthwaighte@gmail.com>
2024-06-13 09:37:55 +1000

    Install pip

--- shell.nix
+++ shell.nix
@@ -1,12 +1,20 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
   pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-
-pkgs.mkShellNoCC {
+in pkgs.mkShellNoCC {
   packages = with pkgs; [
     cowsay
     lolcat
+    neovim
+    (python312.withPackages (ps: with ps; [
+      # Setup pip
+      pip
+    ]))
+    cargo
+    rustc
+    nodejs
+    podman
+    git
   ];
 
   GREETING = "Hello, Nix!";
