899ea0d25c615a164b3766200b85ae235507b9ce
alecbass <alec.bassingthwaighte@gmail.com>
2025-01-01 11:20:42 +1100

    Build: master-1735690842

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#
+# Copies the current OS nixOS configuration and commits it
+# Inspiration: https://www.youtube.com/watch?v=5aBUsLQVZYg
+#
+
+# No errors!!!!!!!
+set -e
+
+# Get the current git branch
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Get the current UNIX timestamp
+TIMESTAMP=$(date +%s)
+
+TAG="${BRANCH}-${TIMESTAMP}"
+
+# Copy the current configuration
+cp /etc/nixos/configuration.nix .
+
+# Add the configuration in case it doesn't exist for some reason
+git add configuration.nix
+
+git commit -m "Build: ${TAG}"
+
