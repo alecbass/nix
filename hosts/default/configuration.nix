54eba39ea42c549c36df4c5269700e8d6136d591
alecbass <alec.bassingthwaighte@gmail.com>
2025-08-11 14:21:50 +1000

    Remove vscode

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -391,29 +391,6 @@ in
     usbutils # Unsurprisingly, USB utilities
     alsa-utils # Sound and volume utilities
 
-    # Editing
-    # dotnetCorePackages.sdk_9_0
-    vscode # To use while razor LSPs don't really work
-    (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
-        bbenoist.nix
-        ms-python.python
-        ms-azuretools.vscode-docker
-        ms-vscode-remote.remote-ssh
-        ms-dotnettools.csharp
-        ms-dotnettools.csdevkit
-        ms-dotnettools.vscodeintellicode-csharp
-        ms-dotnettools.vscode-dotnet-runtime
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "remote-ssh-edit";
-          publisher = "ms-vscode-remote";
-          version = "0.47.2";
-          sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-        }
-      ];
-    })
-
     # Wayland-specific
     hyprshot
     hypridle
@@ -424,6 +401,7 @@ in
     dunst
     wl-clipboard
     swaynotificationcenter
+    hyprpaper # Background image
 
     # Networking
     networkmanagerapplet
