234ddf29760d12cfb07254f058359a3ce93cc7b4
alecbass <alec.bassingthwaighte@gmail.com>
2025-10-10 14:43:56 +1100

    Remove rzls comments

--- modules/rzls/package.nix
+++ modules/rzls/package.nix
@@ -7,15 +7,6 @@
 }:
 let
   pname = "rzls";
-  # dotnet-sdk =
-  #   with dotnetCorePackages;
-  #   sdk_9_0_1xx
-  #   // {
-  #     inherit (sdk_8_0)
-  #       packages
-  #       targetPackages
-  #       ;
-  #   };
 
   dotnet-sdk =
     with dotnetCorePackages;
@@ -31,16 +22,12 @@ buildDotnetModule {
   src = fetchFromGitHub {
     owner = "dotnet";
     repo = "razor";
-    # rev = "2798396c3481573aa49f9c792179ebbb5e183dca";
     rev = "90c12b14924f312df21758d63d7db6ff0290fa5c"; # main
 
-    # hash = "sha256-tPROplLbKmWTbm0r3844zJX3mUQfIWv9jzjcflM5WJk=";
     hash = "sha256-a0Mg5WozSK/AqSyevkZqYQ4z87GsZYJWvNKzVDc/3Vo="; # main
   };
 
-  # version = "9.0.0-beta.25428.3";
   version = "9.0.109";
-  # version = "9.0.0-beta.25111.5";
   projectFile = "src/Razor/src/rzls/rzls.csproj";
   useDotnetFromEnv = true;
   nugetDeps = ./deps.json;
