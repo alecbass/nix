863d64ee957ea902aa18c3d22b71ee4174494162
alecbass <alec.bassingthwaighte@gmail.com>
2026-05-06 15:39:18 +1000

    Take llama-server model as arg

--- modules/run-llama/run-llama.sh
+++ modules/run-llama/run-llama.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 
-context_size=${1:-40960}
+coder_model="unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF"
+default_model="unsloth/Qwen3.6-27B-GGUF"
+
+model_name=${1:-$default_model}
+context_size=${2:-40960}
 
 echo "Running LLM"
+echo "Model: $model_name"
 echo "Context size: $context_size"
 
-coder_model="unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF"
-model="unsloth/Qwen3.6-35B-A3B-GGUF"
-
 llama-server \
-  --hf-repo $model \
+  --hf-repo "$model_name" \
   --n-gpu-layers 999 \
   --ubatch-size 768 \
   --temp 0.2 \
