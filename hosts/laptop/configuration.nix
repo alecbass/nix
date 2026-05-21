9437c876d0191754f9847ca95ac2affa0e370b3b
alecbass <alec.bassingthwaighte@gmail.com>
2026-05-21 11:01:54 +1000

    Fix laptop packages

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, lib, pkgs, inputs, options, customSddmTheme, probeRsRules, packages, ... }:
+{ config, lib, pkgs, inputs, options, customSddmTheme, probeRsRules, packages, nixosPermittedInsecurePackages, ... }:
 let
   username = "alec";
   userDescription = "Alec Bassingthwaighte";
