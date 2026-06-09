d2e6588a9451d77ebc993967d02bd00c694798a4
alecbass <alec.bassingthwaighte@gmail.com>
2026-06-09 11:07:14 +1000

    Remove unused dotfiles

--- hosts/home.nix
+++ hosts/home.nix
@@ -15,33 +15,7 @@ in {
     file = {
       # TODO: Use my dotfiles repository
       # # Hyprland Config
-      # ".config/hypr".source = ../dotfiles/.config/hypr;
-      # wlogout icons
       ".config/wlogout/icons".source = ../config/wlogout;
-      #
-      # # Top Level Files symlinks
-      # ".zshrc".source = ../dotfiles/.zshrc;
-      # ".gitconfig".source = ../dotfiles/.gitconfig;
-      # ".ideavimrc".source = ../dotfiles/.ideavimrc;
-      # ".nirc".source = ../dotfiles/.nirc;
-      # ".local/bin/wallpaper".source = ../dotfiles/.local/bin/wallpaper;
-      #
-      # # Config directories
-      # ".config/alacritty".source = ../dotfiles/.config/alacritty;
-      # ".config/dunst".source = ../dotfiles/.config/dunst;
-      # ".config/fastfetch".source = ../dotfiles/.config/fastfetch;
-      # ".config/kitty".source = ../dotfiles/.config/kitty;
-      # ".config/mpv".source = ../dotfiles/.config/mpv;
-      # ".config/tmux/tmux.conf".source = ../dotfiles/.config/tmux/tmux.conf;
-      # ".config/waybar".source = ../dotfiles/.config/waybar;
-      # ".config/yazi".source = ../dotfiles/.config/yazi;
-      # ".config/wezterm".source = ../dotfiles/.config/wezterm;
-      # ".config/ghostty".source = ../dotfiles/.config/ghostty;
-      # ".config/prettier".source = ../dotfiles/.config/prettier;
-      #
-      # # Individual config files
-      # ".config/kwalletrc".source = ../dotfiles/.config/kwalletrc;
-      # ".config/starship.toml".source = ../dotfiles/.config/starship.toml;
       ".config/rofi/config-emoji.rasi".text = "";
       ".config/rofi/config-long.rasi".text = "";
 
