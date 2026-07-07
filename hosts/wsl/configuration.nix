b8ca6a1a978de6cc47af835c5f49b8f69f9cfc54
alecbass <alec.bassingthwaighte.com>
2026-07-07 12:49:19 +1000

    Working networking, not SSH hangs

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -2,7 +2,14 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ lib, pkgs, config, nixos-wsl, packages, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  nixos-wsl,
+  packages,
+  ...
+}:
 let
   user = import ../user.nix { inherit packages; };
 in
@@ -18,20 +25,27 @@ in
   wsl = {
     enable = true;
     defaultUser = user.userName; # Set your primary login username - from user.nix
-    
+
     # Optional: Integrate Windows PATH into Linux
-    wslConf.interop.appendWindowsPath = true; 
+    wslConf.interop.appendWindowsPath = true;
+    wslConf.network.generateResolvConf = false;
   };
+  networking.enableIPv6 = false;
+  networking.networkmanager.enable = lib.mkForce false;
+  networking.nameservers = [ "8.8.8.8" "1.1.1.1" "8.8.4.4" ];
+  networking.resolvconf.enable = true;
+  boot.kernel.sysctl."net.ipv6.conf.eth0.disable_ipv6" = true;
 
   # WSL handles everything else, including hardware configuration
   # networking = nixos-wsl.nixosModules.default.options.wsl.config.networking; # Let Windows handle the networking
   # networking = {
-    # dhcp is handled by windows
-    # dhcpcd.enable = false;
+  # dhcp is handled by windows
+  # dhcpcd.enable = false;
 
-    # disable resolvconf if WSL is managing it
-    # resolvconf.enable = lib.mkIf config.wsl.wslConf.network.generateResolvConf false;
+  # disable resolvconf if WSL is managing it
+  # resolvconf.enable = lib.mkIf config.wsl.wslConf.network.generateResolvConf false;
   # };
+  programs.ssh.startAgent = lib.mkForce false;
 
   #
   # Hardware
