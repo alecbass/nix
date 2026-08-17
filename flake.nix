493fc4d083088db579f66dcf688be6d9ad7a4423
alecbass <alec.bassingthwaighte@gmail.com>
2026-08-17 18:06:51 +1000

    Update!

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     let
       nixosSystem = "x86_64-linux"; # I only run NixOS on x86 machines
       nixosPermittedInsecurePackages = [
-        "broadcom-sta-6.30.223.271-63-6.18.42"
+        "broadcom-sta-6.30.223.271-63-6.18.44"
       ];
 
       baseOsConfig = { ... }: {
