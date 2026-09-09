2940b14e5f9b50751e7c4de84fa0e12cceb481a6
alecbass <alec.bassingthwaighte@gmail.com>
2026-09-09 19:32:03 +1000

    Gemma testing

--- modules/run-llama/run-llama.sh
+++ modules/run-llama/run-llama.sh
@@ -11,6 +11,8 @@ gpu_layers=${3:-64}
 
 model_name="unsloth/gemma-4-E2B-it-GGUF"
 model_name="ggml-org/Qwen2.5-Coder-1.5B-Q8_0-GGUF"
+model_name="unsloth/gemma-4-26B-A4B-it-qat-GGUF"
+
 
 echo "Running LLM"
 echo "Model: $model_name"
@@ -18,7 +20,6 @@ echo "Context size: $context_size"
 
 if [[ $model_name == "ggml-org/Qwen2.5-Coder-1.5B-Q8_0-GGUF" ]]; then
     echo "Running Qwen2.5 Coder for autocompletion."""
-
     llama-server \
       --hf-repo "$model_name" \
       --temp 1.0 \
@@ -32,14 +33,19 @@ if [[ $model_name == "ggml-org/Qwen2.5-Coder-1.5B-Q8_0-GGUF" ]]; then
       --ctx-size 0 \
       --cache-reuse 256
 else
+    gpu_layers=8
     llama-server \
       --hf-repo "$model_name" \
       --temp 1.0 \
       --host 127.0.0.1 \
-      --gpu-layers "$gpu_layers" \
       --jinja \
       --port 8012 \
+      -ngl 99 \
+      -fa on \
+      -ub 1024 \
+      -b 1024 \
       --ctx-size "$context_size" \
+      --gpu-layers "$gpu_layers" \
       --cache-reuse 256
 fi
 
